50b2f868559ce7275039a3dd480c9f205adf7135
ebedoyaalzate <esteban.bedoyaa@udea.edu.co>
2019-03-06 22:56:06 -0500

    Se agrega validacion y constantes

--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import WeatherIcons from 'react-weathericons'
+import PropTypes from 'prop-types'
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY
+} from '../constanst/weather'
 
 const icons ={
-    sunny: "day-sunny",
-    fog: "day-fog"
+    [CLOUD]: 'cloud',
+    [CLOUDY]: 'cloudy',
+    [SUN]: "day-sunny",
+    [RAIN]: 'rain',
+    [SNOW]: 'snow',
+    [WINDY]: 'windy'
 }
 const getWeatherIcon = weatherState=>{
     const icon = icons[weatherState]
     if(icon)
         return <WeatherIcons name={icon} size="5x"/>
     else
-        return <WeatherIcons name={"day-fog"} size="5x"/>
+        return <WeatherIcons name={"day-sunny"} size="5x"/>
 }
 const WeatherTemperature = ({temperature, weatherState})=>(
     <div>
@@ -20,5 +33,10 @@ const WeatherTemperature = ({temperature, weatherState})=>(
         <span>{`${temperature}C°`} </span>
     </div>   
 )
+
+WeatherTemperature.propTypes={
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired
+}
 
 export default WeatherTemperature
